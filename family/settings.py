d82da232ab04befd07174b7331071bdd03f2cb00
sansu <0712lgy@hanmail.net>
2018-03-12 18:13:47 +0900

    csrf 402 error

--- family/settings.py
+++ family/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
+    
 ]
 
 ROOT_URLCONF = 'family.urls'
