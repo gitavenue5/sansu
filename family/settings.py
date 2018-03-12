27aade1dff14d56322337aef03c90d884a197cb0
sansu <0712lgy@hanmail.net>
2018-03-12 17:09:31 +0900

    403 csrf 모바일 error

--- family/settings.py
+++ family/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # 모바일 403 csrf error
+    'djago.middleware.csrf.CsrfViewMiddleware',
 ]
 
 ROOT_URLCONF = 'family.urls'
