1d13feafaee584604172b81cc54acb15bf6ff535
sansu <0712lgy@hanmail.net>
2018-03-19 09:24:49 +0900

    날짜 계산

--- family/views.py
+++ family/views.py
@@ -4,10 +4,32 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
 
+from datetime import datetime, date
     
 # 처음페이지 함수
 class SansuTemplateView(TemplateView):
     template_name = 'main_layout.html'
+
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        
+       # 병원 진료 예약일
+       # 안과
+        op_1 = datetime(2018,5,11)
+        op_2 = datetime.now()        
+        context['op_3'] = op_1 - op_2   
+
+        
+        # 요양병원 입원일수
+        convalescentHospital_1 = datetime(2017,8,29)
+        convalescentHospital_2 = datetime.now()
+        context['convalescentHospital_3'] = convalescentHospital_2 - convalescentHospital_1
+
+
+        return context
+
+    
 
 # 회원가입 함수
 class UserCreateView(CreateView):
