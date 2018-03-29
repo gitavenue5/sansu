3150ddcbe89d9efe36443e60083ab7e0d5b08d32
sansu <0712lgy@hanmail.net>
2018-03-29 15:39:53 +0900

    daily

--- family/views.py
+++ family/views.py
@@ -4,7 +4,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
 
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import re
     
 # 처음페이지 함수
@@ -14,6 +14,13 @@ class SansuTemplateView(TemplateView):
     
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
+
+        # 전대병원 입원기간
+        cnuh_1 = date(2017,6,26)
+        cnuh_2 = date(2017,8,28)
+        cnuh_3 = cnuh_2 - cnuh_1
+        # 입원기간
+        context['cnuh_3'] = cnuh_2 - cnuh_1
         
        # 요양병원 입원일수 아래것이 단순함.
         #convalescentHospital_1 = datetime(2017,8,29)
@@ -22,7 +29,11 @@ class SansuTemplateView(TemplateView):
 
         convalescentHospital_1 = date(2017,8,29)
         convalescentHospital_2 = date.today()
+        convalescentHospital_3 = convalescentHospital_2 - convalescentHospital_1
         context['convalescentHospital_3'] = convalescentHospital_2 - convalescentHospital_1
+
+        # 총 입원일
+        context['hospitalization'] = (cnuh_3 + convalescentHospital_3) + timedelta(days=1)
        
        # 병원 진료 예약일
        # 안과
