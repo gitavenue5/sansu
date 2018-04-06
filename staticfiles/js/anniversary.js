abf5025ebb880f897dcb04c2f2b45a3ada93d840
sansu <0712lgy@hanmail.net>
2018-04-06 09:56:04 +0900

    4-6-birthday

--- staticfiles/js/anniversary.js
+++ staticfiles/js/anniversary.js
@@ -1,15 +1,25 @@
-$(document).ready(function(){
-    $.getJSON('/api/anniversary_api', function(data){
-        if( data.length > 0){
-            $('#uuu').empty();            
-            $.each(data, function(){
-                
-                var kk = '<ul><li>' + this['anniversary_date'] + '<li></li>' + this['anniversary_name'] + '</li></ul>';
-                
-                     //$('#uuu').append(kk);
-          
-                
-            })//each
-        }// if
-    });// getJson
-})// ready
+ 
+    $(document).ready(function(){ 
+        $.getJSON('/api/anniversary_api', function(data){
+            if(data.length>0){
+                   
+               // $('#birthday').empty();  
+                var now = new Date();
+                var d = now.getFullYear();
+                    d += '-' + (now.getMonth() + 1) ;
+                    d += '-' + now.getDate();  
+                    
+                $.each(data, function(){
+                    var b = this['anniversary_date'];           
+                                            
+                        if(b==d){
+                            $('#birthday').show().append(this['anniversary_name'] + "님 생일 축하합니다!") ;
+                            
+                        }//if
+                        
+                    
+                });// each
+            };//if
+        });//getjson
+    });// ready
+    
