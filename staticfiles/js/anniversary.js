71c53f701b54cd6525918708ec30a58a0f966209
sansu <0712lgy@hanmail.net>
2018-04-17 01:31:20 +0900

    4-17-2

--- staticfiles/js/anniversary.js
+++ staticfiles/js/anniversary.js
@@ -17,8 +17,8 @@
                     day = "0" + day;
                 }// if getday
 
-
                 var today = year + '-' + month + '-' + day;
+               
                 $.each(data, function(){
                     var b = this['anniversary_date']; 
                     var c = this['anniversary_name'];  
@@ -31,12 +31,12 @@
                         }//if
 
                         if((b==today)&&(c=='어머님 생신')){                            
-                            $('#birthday1').show().append( d + '을 축하드립니다!'); // 다른 <div>로 대체
+                            $('#birthday1').show().append( d + '을 축하드리며, 쾌유를 빕니다!'); // 다른 <div>로 대체
                             //$('#birthday1').show().replaceWith(d + '을 축하드립니다!'); ==> css가 안먹히고, bootstarp class가 안멱힌다.
                             //$('#birthday').show().replaceWith(this['anniversary_name']+'<h2>축하드립니다</h2>'); 간단한 replaceWith 사용법                           
                         }// if end
 
-                        if((b==today)&&(c=='아버님 기일')){                            
+                        if((b==today)&&(c=='아버님 기일')){                           
                             $('#birthday2').show().append( d + '입니다!'); // 다른 <div>로 대체
                             //$('#birthday1').show().replaceWith(d + '을 축하드립니다!'); ==> css가 안먹히고, bootstarp class가 안멱힌다.
                             //$('#birthday').show().replaceWith(this['anniversary_name']+'<h2>축하드립니다</h2>'); 간단한 replaceWith 사용법                           
